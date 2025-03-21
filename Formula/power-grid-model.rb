9ca43321118184b00b91af7df3a0dcb4a25afd0e
Tony Xiang <19280867+TonyXiang8787@users.noreply.github.com>
2025-03-21 19:46:46 +0100

    start formula
    Signed-off-by: Tony Xiang <19280867+TonyXiang8787@users.noreply.github.com>

---
+++ Formula/power-grid-model.rb
@@ -0,0 +1,38 @@
+# SPDX-FileCopyrightText: Contributors to the Power Grid Model project <powergridmodel@lfenergy.org>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+class PowerGridModel < Formula
+  desc "Python/C++ library for distribution power system analysis"
+  homepage ""
+  url "https://github.com/PowerGridModel/power-grid-model/archive/refs/tags/v1.10.69.tar.gz"
+  sha256 "870107627d9d3cc6ed2eeed382d12e9238da9ed434a18acb995e9e6c6ba1d0c3"
+  license "MPL-2.0"
+
+  depends_on "cmake" => :build
+
+  # Additional dependency
+  # resource "" do
+  #   url ""
+  #   sha256 ""
+  # end
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test power-grid-model`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system bin/"program", "do", "something"`.
+    system "false"
+  end
+end
